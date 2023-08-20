26159a8184de111a8523642463aa4f507ae37fee
scott.sibole@gmail.com <scott.sibole@gmail.com>
2023-08-20 12:01:56 -0600

    Add DAP

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -142,8 +142,18 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
-    -- cond = function()
-    --   return require("lazyvim.util").has("nvim-cmp")
-    -- end,
+  },
+  { "mfussenegger/nvim-dap", opts = {} },
+  {
+    "mfussenegger/nvim-dap-python",
+  -- stylua: ignore
+  keys = {
+    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
+  },
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      require("dap-python").setup(path .. "/venv/bin/python")
+    end,
   },
 }
