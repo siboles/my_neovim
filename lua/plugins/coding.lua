1b6f2e120f9593379981683ab46c565167b8b4d3
scott.sibole@gmail.com <scott.sibole@gmail.com>
2023-09-24 14:56:01 -0600

    Added LSP and DAP for clang

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,4 +1,34 @@
 return {
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    config = function() end,
+    opts = {
+      inlay_hints = {
+        inline = false,
+      },
+      ast = {
+        --These require codicons (https://github.com/microsoft/vscode-codicons)
+        role_icons = {
+          type = "",
+          declaration = "",
+          expression = "",
+          specifier = "",
+          statement = "",
+          ["template argument"] = "",
+        },
+        kind_icons = {
+          Compound = "",
+          Recovery = "",
+          TranslationUnit = "",
+          PackExpansion = "",
+          TemplateTypeParm = "",
+          TemplateTemplateParm = "",
+          TemplateParamObject = "",
+        },
+      },
+    },
+  },
   -- better text objects
   {
     "echasnovski/mini.ai",
@@ -143,17 +173,56 @@ return {
   {
     "hrsh7th/nvim-cmp",
   },
-  { "mfussenegger/nvim-dap", opts = {} },
   {
-    "mfussenegger/nvim-dap-python",
-  -- stylua: ignore
-  keys = {
-    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
-    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
-  },
-    config = function()
-      local path = require("mason-registry").get_package("debugpy"):get_install_path()
-      require("dap-python").setup(path .. "/venv/bin/python")
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      if not dap.adapters["codelldb"] then
+        require("dap").adapters["codelldb"] = {
+          type = "server",
+          host = "localhost",
+          port = "${port}",
+          executable = {
+            command = "codelldb",
+            args = {
+              "--port",
+              "${port}",
+            },
+          },
+        }
+      end
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process",
+            processId = require("dap.utils").pick_process,
+            cwd = "$workspaceFolder",
+          },
+        }
+      end
     end,
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      automatic_installation = true,
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+        "debugpy",
+      },
+    },
   },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,6 +14,9 @@ return {
         "ruff",
         "isort",
         "pyright",
+        "ocaml-lsp",
+        "ocamlformat",
+        "codelldb",
       },
     },
   },
@@ -34,13 +37,48 @@ return {
       inlay_hints = { enabled = true },
       ---@type lspconfig.options
       servers = {
-        clangd = {},
+        clangd = {
+          keys = {
+            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatue = true,
+          },
+        },
         cssls = {},
         dockerls = {},
         svelte = {},
         html = {},
         gopls = {},
         marksman = {},
+        ocamllsp = {},
         pyright = {
           settings = {
             python = {
@@ -53,7 +91,13 @@ return {
         },
         rust_analyzer = {},
       },
-      setup = {},
+      setup = {
+        clangd = function(_, opts)
+          local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+          return false
+        end,
+      },
     },
   },
 
@@ -82,6 +126,7 @@ return {
           nls.builtins.formatting.isort,
           nls.builtins.formatting.black,
           nls.builtins.diagnostics.ruff,
+          nls.builtins.formatting.ocamlformat,
           -- nls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length=88", "--ignore=E203" } }),
         },
       }
