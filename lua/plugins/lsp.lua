12301db284df323ee15f80f206ac6bbade28c65d
Scott Sibole <scott.sibole@gmail.com>
2023-10-10 11:37:05 -0600

    Updated plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -110,18 +110,17 @@ return {
 
   -- null-ls
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
     opts = function()
       local nls = require("null-ls")
       return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
           nls.builtins.formatting.fish_indent,
           nls.builtins.diagnostics.fish,
           nls.builtins.formatting.stylua,
-          nls.builtins.formatting.prettierd.with({ filetypes = { "markdown", "json" } }),
+          nls.builtins.formatting.prettierd.with({ filetypes = { "markdown", "json", "rust" } }),
           nls.builtins.diagnostics.markdownlint,
           nls.builtins.formatting.isort,
           nls.builtins.formatting.black,
